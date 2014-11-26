464677fffb66a2dfc449c17b61000b53c9aeb097
Michel Lemay <mlemay@gmail.com>
2014-11-26 16:02:23 +0000

    Modified setup script and chmod of script files.

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -2,17 +2,20 @@
 pig -version
 hadoop version
 # We use elinks to look at the job tracker and screen to collaboratively debug stuff
-sudo apt-get install elinks
-sudo apt-get install screen
+#sudo apt-get install elinks
+#sudo apt-get install screen
+sudo yum -y install elinks
+sudo yum -y install screen
+sudo yum -y install vim
 # some dependencies require maven3. will install
 sudo mkdir -p /opt/tools
 sudo chown hadoop /opt/tools
 cd /opt/tools
-wget ftp://mirror.csclub.uwaterloo.ca/apache/maven/maven-3/3.0.4/binaries/apache-maven-3.0.4-bin.tar.gz
-tar -xzvf apache-maven-3.0.4-bin.tar.gz
-export MAVEN_HOME=/opt/tools/apache-maven-3.0.4
-export M2_HOME=/opt/tools/apache-maven-3.0.4
-export M2=/opt/tools/apache-maven-3.0.4/bin
-export PATH=/opt/tools/apache-maven-3.0.4/bin:$PATH
+wget ftp://mirror.csclub.uwaterloo.ca/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz
+tar -xzvf apache-maven-3.2.3-bin.tar.gz
+export MAVEN_HOME=/opt/tools/apache-maven-3.2.3
+export M2_HOME=/opt/tools/apache-maven-3.2.3
+export M2=/opt/tools/apache-maven-3.2.3/bin
+export PATH=/opt/tools/apache-maven-3.2.3/bin:$PATH
 mvn --version
-echo -e "export MAVEN_HOME=/opt/tools/apache-maven-3.0.4 \n export M2_HOME=/opt/tools/apache-maven-3.0.4 \n export M2=/opt/tools/apache-maven-3.0.4/bin \n export PATH=/opt/tools/apache-maven-3.0.4/bin:\$PATH" >> ~/.bashrc
+echo -e "export MAVEN_HOME=/opt/tools/apache-maven-3.2.3 \n export M2_HOME=/opt/tools/apache-maven-3.2.3 \n export M2=/opt/tools/apache-maven-3.2.3/bin \n export PATH=/opt/tools/apache-maven-3.2.3/bin:\$PATH" >> ~/.bashrc
